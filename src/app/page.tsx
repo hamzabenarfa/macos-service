b19269c10fa5088b84acbc3ef390122ee9cb1875
Hamza <hamzabenarfa4@gmail.com>
2023-06-17 18:56:14 +0100

    added things to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Landing from "./components/Landing/Landing";
+import Down from "./components/down/Down";
+
 export default function Home() {
   return (
-    <main>
-      
-      <Landing />
-    </main>
+    <div>
+      <main className="relative h-[200vh] bg-[#E7ECEE]">
+        <Landing />
+      </main>
+
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+        <div className="space-y-10 py-16">
+          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+            Nos Services
+          </h1>
+        </div>
+      </section>
+      <section className="relative z-40 min-h-screen bg-[#f5d142]">
+        <div className="space-y-10 py-16">
+          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+            Témoignages Clients
+          </h1>
+        </div>
+      </section>
+    </div>
   );
 }
