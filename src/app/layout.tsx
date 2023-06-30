fbe6f1b5f2732289283023dc817f9932228c750a
Hamza <hamzabenarfa4@gmail.com>
2023-06-30 18:26:06 +0100

    fixed gtag?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,15 +26,16 @@ export default function RootLayout({
         <meta name="description" content={metadata.description} />
 
         <link rel="icon" href="/icon.png"  />
-        <Script id="google-analytics">
+       
+        </Head>
+      <body className={inter.className}>
+      <Script id="google-analytics">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KT5B8MX');
 `}</Script>
-        </Head>
-      <body className={inter.className}>
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT5B8MX"
 height="0" width="0" style={{"display": "none", "visibility": "hidden"}}></iframe></noscript>
         <Navbar />
