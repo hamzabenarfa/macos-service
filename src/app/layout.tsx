d9e6ac0a327851d1e33343136909b5112e467247
Hamza <hamzabenarfa4@gmail.com>
2023-07-01 12:03:28 +0100

    fixed gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Script from "next/script";
 
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Script from 'next/script'
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata  = {
-  title: 'macos-service',
-  description: 'apple website',
-}
+export const metadata = {
+  title: "macos-service",
+  description: "apple website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr" className="scroll-smooth">
@@ -24,25 +23,31 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-
-        <link rel="icon" href="/icon.png"  />
-       
-        </Head>
+        <Script id="google-analytics">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com
+            m.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KT5B8MX');
+          `}
+        </Script>
+        <link rel="icon" href="/icon.png" />
+      </Head>
       <body className={inter.className}>
-      
         <Navbar />
         {children}
         <Footer />
-        <Script id="google-analytics">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KT5B8MX');
-`}</Script>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT5B8MX"
-height="0" width="0" style={{"display": "none", "visibility": "hidden"}}></iframe></noscript>
-        </body>
+        
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KT5B8MX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+      </body>
     </html>
-  )
+  );
 }
