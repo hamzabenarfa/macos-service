a64c1d7d06ba1cdfb1335236e386fceec7b418be
Hamza <hamzabenarfa4@gmail.com>
2023-07-01 23:06:25 +0100

    fixed bar

--- src/app/components/FixedBar.jsx
+++ src/app/components/FixedBar.jsx
@@ -3,7 +3,7 @@ export default function FixedBar() {
   return (
     <div>
 
-      <div className="fixed  bottom-0 left-0 right-0 bg-white/30 backdrop-blur-md md:hidden">
+      <div className="fixed bottom-0 left-0 right-0 p-4  bg-white/30 backdrop-blur-md md:hidden">
         <p className="text-center text-xl font-bold capitalize ">
           <a
             href="tel:0980806764"
