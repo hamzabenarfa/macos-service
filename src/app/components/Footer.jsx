6548351c83cd1621b553a36a5d72d6c51995a541
Hamza <hamzabenarfa4@gmail.com>
2023-07-01 12:55:23 +0100

    footer finished

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <footer>
         <div className="grid-row-2 grid h-full bg-gradient-to-t from-gray-200 to-gray-50 ">
           <div className="flex flex-row  items-center justify-between space-x-4">
-            <div className="flex w-full justify-center space-y-2 bg-teal-300 p-4">
+            <div className="flex w-full justify-center space-y-2 p-4">
               <div className="flex flex-col items-start">
                 <Link href="/macbookpro" className="footerNav">
                   <img
