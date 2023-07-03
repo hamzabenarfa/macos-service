e835b9c83c222f0728cf462e7065d220893e7105
Hamza <hamzabenarfa4@gmail.com>
2023-07-03 17:45:11 +0100

    fixed gtm

--- src/app/components/FixedBar.jsx
+++ src/app/components/FixedBar.jsx
@@ -2,7 +2,7 @@
 export default function FixedBar() {
   return (
     <>
-      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center gap-2  bg-white p-4 shadow-2xl md:hidden">
+      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center gap-2 bg-white p-4 shadow-2xl md:hidden">
         <p className="text-center text-xl font-bold capitalize ">
           <a
             href="tel:0980806764"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Script from "next/script";
+import Navbar from "./components/Navbar";
 import FixedBar from "./components/FixedBar";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -20,36 +18,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <Head>
+      <head>
+        <Script id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KT5B8MX');`,
+          }}
+        ></Script>
+
         <meta charSet="utf-8" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        
+
         <link rel="icon" href="/icon.png" />
-      </Head>
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
         <FixedBar />
         <Footer />
-        
-        <Script id="google-analytics">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com
-            m.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KT5B8MX');
-          `}
-        </Script>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KT5B8MX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+    
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT5B8MX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
       </body>
     </html>
   );
